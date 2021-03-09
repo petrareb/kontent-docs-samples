196c9396df8fe53e4bcdfc86e101bdf7453d4f15
Jan Cerman <janc@kentico.com>
2021-03-09 13:31:38 +0100

    KST-23 Try sync small change

--- js/getting-content/FilterItems.js
+++ js/getting-content/FilterItems.js
@@ -2,7 +2,7 @@
 // Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
 const KontentDelivery = require('@kentico/kontent-delivery');
 
-const deliveryClient = new KontentDelivery.DeliveryClient({
+TESTconst deliveryClient = new KontentDelivery.DeliveryClient({
   projectId: '8d20758c-d74c-4f59-ae04-ee928c0816b7',
   typeResolvers: [
     // Create strongly typed models according to https://docs.kontent.ai/strongly-typed-models
